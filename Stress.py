c81d3faa8a74370787e3f2489c3f3984c0a6e024
Sergey Murashov <smurashov@mirantis.com>
2013-11-27 10:40:05 +0400

    Add get_glance_images method

--- Stress.py
+++ Stress.py
@@ -2,6 +2,7 @@ from keystoneclient.v2_0 import client as ksclient
 from multiprocessing import Process
 from cinderclient import client as cindercl
 import novaclient.v1_1.client as nvclient
+import glanceclient.v2.client as glclient
 import time
 import random
 import os
@@ -11,12 +12,16 @@ class Mirantis(Process):
 
     def __init__(self, numb, user, password, tenant, keystone_url):
         super(Mirantis, self).__init__()
-        ksclient.Client(username=user, password=password,
-                        tenant_name=tenant, auth_url=keystone_url)
+        keystone = ksclient.Client(username=user, password=password,
+                                   tenant_name=tenant, auth_url=keystone_url)
         self.nova = nvclient.Client(user, password, tenant, keystone_url,
                                     service_type="compute")
         self.cinder = cindercl.Client('1', user, password,
                                       tenant, keystone_url)
+        glance_endpoint = keystone.service_catalog.url_for(service_type='image',
+                                                   endpoint_type='publicURL')
+        self.glance = glclient.Client(glance_endpoint,
+                                      token=keystone.auth_token)
         self.numb = numb
         self.points = []
 
@@ -65,13 +70,18 @@ class Mirantis(Process):
 
     @timecheck
     def get_servers_list(self):
-        servers = self.nova.servers.list()
-        return servers
+        return self.nova.servers.list()
+
+    @timecheck
+    def get_images_list(self):
+        return self.glance.images.list()
 
     def run(self):
         k = random.randint(51, 100)
         if k < 50:
             instance = self.create_instance()
             self.delete_instance(instance)
+        elif k > 101:
+            self.get_images_list()
         elif k < 100:
             self.get_servers_list()
